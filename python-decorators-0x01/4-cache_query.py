c89f6d76746d765d72b959d5dc57da910c909bb4
danielkingori <kingndiritu@gmail.com>
2025-06-01 20:48:48 +0300

    fix: cache

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,46 @@
+import time
+import sqlite3 
+import functools
+
+
+query_cache = {}
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('my_database.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            conn.close()
+    return wrapper
+
+def cache_query(func):
+   
+    @functools.wraps(func)
+    def wrapper(sql_query, *args, **kwargs):
+        if sql_query in query_cache:
+            print("Returning cached result for query.")
+            return query_cache[sql_query]
+        
+        print("Executing and caching new query.")
+        result = func(sql_query, *args, **kwargs)
+        query_cache[sql_query] = result
+        return result
+    return wrapper
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+#### First call will cache the result
+users = fetch_users_with_cache(query="SELECT * FROM users")
+
+#### Second call will use the cached result
