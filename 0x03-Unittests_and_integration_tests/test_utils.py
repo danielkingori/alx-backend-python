5091ecadba225db706e3ec74969f77d608b531f1
danielkingori <kingndiritu@gmail.com>
2025-06-01 23:41:23 +0300

    fix: test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,17 +9,17 @@ from utils import access_nested_map  # importing the access_nested_map function
 from utils import get_json #importing get_json function
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test class for access_nested_map."""
+# class TestAccessNestedMap(unittest.TestCase):
+#     """Test class for access_nested_map."""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns expected result."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+#     @parameterized.expand([
+#         ({"a": 1}, ("a",), 1),
+#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#         ({"a": {"b": 2}}, ("a", "b"), 2),
+#     ])
+#     def test_access_nested_map(self, nested_map, path, expected):
+#         """Test access_nested_map returns expected result."""
+#         self.assertEqual(access_nested_map(nested_map, path), expected)
     
 
 
@@ -58,11 +58,11 @@ class TestAccessNestedMap(unittest.TestCase):
 
     # New exception test method
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+    ({}, ("a",)),
+    ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Test that access_nested_map raises KeyError with correct message."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), repr(path[len(context.exception.args[0].split('.')[0].split('/')[-1]):][0]))
+        self.assertEqual(str(context.exception), repr(path[-1]))
