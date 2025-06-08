c36ef9727f0ecef528600b25b8f6669ce2a08ae1
danielkingori <kingndiritu@gmail.com>
2025-06-08 20:21:00 +0300

    fix:utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -50,6 +50,7 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     """Tests for the memoize decorator."""
 
@@ -72,4 +73,3 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
             mock_method.assert_called_once()
-
